c816bb0251023999326173f33a8271d876b1fb63
Vicky <rofiqiatapsil37@gmail.com>
2024-11-09 13:20:18 +0700

    membuat validasi format email

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // script.js
+document.getElementById('registrationForm').addEventListener('submit', function(event) {
+    var email = document.getElementById('email').value;
+    var no_hp = document.getElementById('no_hp').value;
+
+    // Validasi format email
+    var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    if (!emailPattern.test(email)) {
+        alert('Email tidak valid');
+        event.preventDefault();
+    }
